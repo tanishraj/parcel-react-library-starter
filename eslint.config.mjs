83fda9594d55d2de9b3d07a30bde476a87706651
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-16 15:16:01 +0400

    feat: Eslint Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,28 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ['node_modules', 'dist'] },
   { files: ['**/*.{js,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  configPrettier,
+
+  {
+    name: 'ESLint + Prettier',
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...configPrettier.rules,
+      ...pluginPrettier.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
 ];
