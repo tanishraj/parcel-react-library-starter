1cb8694c52a07e1bdccf7607cbe8267ec2bdb32e
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-22 11:41:39 +0400

    feat: exporting utils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // TAILWIND CSS IMPORT
 import './tailwind.css';
 
+// EXPORT CLASSNAMES UTILS
+export * from './utils';
+
 // EXPORT LIBRARY COMPONENTS
 export * from './components';
 export * from './hooks';
