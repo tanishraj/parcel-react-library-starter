6e8ec5a2b04cd36605acfe1ef3fcac047bdb099c
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-24 01:52:09 +0400

    feat: additional vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,15 @@ export default defineConfig({
     outDir: 'dist',
     cssMinify: true,
     sourcemap: true,
+    minify: 'terser',
+    cssCodeSplit: true,
+    reportCompressedSize: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     lib: {
       entry: 'src/index.ts',
       name: 'Parcel React Library Starter',
