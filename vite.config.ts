8b9e121f2e2837b6575c4977285527b6f8c70656
Tanish Raj <learn.tanishraj@gmail.com>
2025-03-24 02:54:36 +0400

    feat: type declaration file during build using vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@
 import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.json' }),
+  ],
   server: {
     port: 3000,
     open: true,
